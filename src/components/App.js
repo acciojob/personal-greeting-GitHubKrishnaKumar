a2c6453407fd77ad2e22b58ca9964145b78aa09a
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2023-07-03 10:54:51 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import { UsageState } from "react";
+import Greeting from "./Greeting";
 const App = () => {
+const [name,setName] = useState('');
+
   return (
     <div>
         {/* Do not remove the main div */}
+      <Greeting/>
     </div>
   )
 }
